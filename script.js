66bda622372ba1cbcdc9aa58290aaff22284db4a
Robert de Grijs <rdgrijs@hotmail.com>
2020-05-21 14:03:17 +0200

    add final game logic

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const container = document.querySelector(".container");
 let currentPlayer = 1;
 const takenSquares = [];
 let winPositions = [];
+const player = document.querySelector('.player');
+const winMessage = document.querySelector('#winmessage');
+const winContainer = document.querySelector('#wincontainer');
+const resetButton = document.querySelector('#resetbutton');
+
+player.textContent = currentPlayer;
+
+resetButton.addEventListener('click', resetGame)
 
 function setupGame() {
     for (let i = 1; i < 65; i++) {
@@ -9,9 +17,11 @@ function setupGame() {
         let square = document.createElement('div');
         square.addEventListener('click', makeMove)
         square.setAttribute('id', [i])
+
         if (i > 56) {
             square.setAttribute('class', 'taken')
         }
+
         container.appendChild(square);
     }
 }
@@ -23,29 +33,81 @@ function makeMove() {
             this.classList.add('p1');
             this.classList.remove('taken');
             currentPlayer = 2;
+            player.textContent = currentPlayer;
+
         } else {
             this.classList.add('p2');
             this.classList.remove('taken')
             currentPlayer = 1;
+            player.textContent = currentPlayer;
+
         }
         if (this.id > 8) {
             let squareTop = document.getElementById(this.id - 8)
             squareTop.classList.add('taken');
         }
-        takenSquares.push(this);
-        console.log('after', takenSquares);
+
+        let takenSquare = {
+            id: "",
+            class: ""
+        }
+        takenSquare.id = this.id;
+        takenSquare.class = this.classList[0];
+        takenSquares.push(takenSquare);
         checkWin();
     }
 }
 
 function checkWin() {
 
-    winPositions.forEach((position) => {
+    let squares = document.querySelectorAll('div');
 
+    for (let i = 0; i < winPositions.length; i++) {
+        let square1 = winPositions[i][0]
+        let square2 = winPositions[i][1]
+        let square3 = winPositions[i][2]
+        let square4 = winPositions[i][3]
+
+        if (
+            squares[square1].classList.contains('p1') &&
+            squares[square2].classList.contains('p1') &&
+            squares[square3].classList.contains('p1') &&
+            squares[square4].classList.contains('p1')
+        ) {
+            winContainer.classList.toggle('showwinner')
+            winMessage.textContent = 'Player 1 wins!';
+            removeListeners(squares);
+        } else if (
+
+            squares[square1].classList.contains('p2') &&
+            squares[square2].classList.contains('p2') &&
+            squares[square3].classList.contains('p2') &&
+            squares[square4].classList.contains('p2')
+
+        ) {
+            winContainer.classList.toggle('showwinner')
+            winMessage.textContent = 'Player 2 wins!';
+            removeListeners(squares);
+        }
     }
-    )
 }
 
+function resetGame() {
+
+    winContainer.classList.toggle('showwinner')
+
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+
+    setupGame();
+}
+
+function removeListeners(squares) {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].removeEventListener('click', makeMove);
+    }
+}
 
 function horizontalCheck() {
     for (let i = 1; i < 64; i += 8) {
@@ -58,6 +120,7 @@ function horizontalCheck() {
         }
     }
 }
+
 function verticalCheck() {
     for (let i = 1; i < 9; i++) {
         for (let y = 0; y < 40; y += 8) {
@@ -70,11 +133,9 @@ function verticalCheck() {
     }
 }
 
-// Function to add diagonal winning posistions to winarray
-function diagonalCheck() {
+function diagonalCheckOne() {
     for (let i = 1; i < 38; i++) {
         let isDiagnonal = true;
-        // Switch to exclude the starting positions of 4 in a row lines that go out of bounds
         switch (i) {
             case 6:
             case 7:
@@ -100,7 +161,36 @@ function diagonalCheck() {
     }
 }
 
-diagonalCheck();
+function diagonalCheckTwo() {
+    for (let i = 4; i < 41; i++) {
+        let isDiagnonal = true;
+        switch (i) {
+            case 9:
+            case 10:
+            case 11:
+            case 17:
+            case 18:
+            case 19:
+            case 25:
+            case 26:
+            case 27:
+            case 33:
+            case 34:
+            case 35: isDiagnonal = false;
+                break;
+        }
+        if (isDiagnonal) {
+            let wincoordinates = [];
+            for (let j = 0; j < 28; j += 7) {
+                wincoordinates.push(i + j);
+            }
+            winPositions.push(wincoordinates);
+        }
+    }
+}
+
+diagonalCheckOne();
+diagonalCheckTwo();
 verticalCheck();
 horizontalCheck();
 
